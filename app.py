4d73c01b88133e0ed5bc0f17891b871018966085
unknown <johnatasr@hotmail.com>
2021-03-07 23:45:57 -0300

    Fin project

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from ursina import Ursina, load_texture, held_keys
+from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
-from voxel import Voxel
 
 jogo = Ursina()
 
@@ -8,15 +7,72 @@ grama_texture = load_texture('assets/grass_block.png')
 pedra_texture = load_texture('assets/stone_block.png')
 madeira_texture = load_texture('assets/brick_block.png')
 lama_texture = load_texture('assets/dirt_block.png')
+ceu_texture = load_texture('assets/skybox.png')
+braco_texture = load_texture('assets/arm_texture.png')
+punch_sound = Audio('assets/punch_sound',loop = False, autoplay = False)
+block = [1, grama_texture]
 
-
-def set_block():
+def update():
     global block
 
-    if held_keys['1']: block = [1, grama_texture]
+    if held_keys['left mouse'] or held_keys['right mouse']:
+        mao.ativo()
+    else:
+        mao.passivo()
+
+    if held_keys['1']: block = block
     if held_keys['2']: block = [2, pedra_texture]
     if held_keys['3']: block = [3, madeira_texture]
     if held_keys['4']: block = [4, lama_texture]
+
+
+class Voxel(Button):
+    def __init__(self, posicao=(0, 0, 0), textura=grama_texture):
+        super().__init__(
+            parent=scene,
+            position=posicao,
+            model='assets/block',
+            origin_y=0.5,
+            texture=textura,
+            color=color.color(0,0,random.uniform(0.9,1)),
+            scale=0.5
+        )
+
+    def input(self, key):
+        if self.hovered:
+            if key == 'left mouse down':
+                if block[0] == 1: Voxel(posicao=self.position + mouse.normal, textura=block[1])
+                if block[0] == 2: Voxel(posicao=self.position + mouse.normal, textura=block[1])
+                if block[0] == 3: Voxel(posicao=self.position + mouse.normal, textura=block[1])
+                if block[0] == 4: Voxel(posicao=self.position + mouse.normal, textura=block[1])
+
+            if key == 'right mouse down':
+                destroy(self)
+
+class Ceu(Entity):
+	def __init__(self):
+		super().__init__(
+			parent = scene,
+			model = 'sphere',
+			texture = ceu_texture,
+			scale = 150,
+			double_sided = True)
+
+class Mao(Entity):
+	def __init__(self):
+		super().__init__(
+			parent = camera.ui,
+			model = 'assets/arm',
+			texture = braco_texture,
+			scale = 0.2,
+			rotation = Vec3(150,-10,0),
+			position = Vec2(0.4,-0.6))
+
+	def ativo(self):
+		self.position = Vec2(0.3,-0.5)
+
+	def passivo(self):
+		self.position = Vec2(0.4,-0.6)
 
 
 for z in range(30):
@@ -24,5 +80,7 @@ for z in range(30):
         Voxel(posicao=(x, 0, z))
 
 jogador = FirstPersonController()
+ceu = Ceu()
+mao = Mao()
 
 jogo.run()
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Minecraft em Python
+
+Pequeno projeto para mostrar e estudar um pouco mais sobre python com jogos. A biblioteca utilizada foi a Ursina como game engine.
+
+###### Atenção! Apenas para propósitos de estudo.
+
+## Pré-requisitos
+
+* Python
+
+## Instalação
+
+1. `cd` na pasta raiz do projeto
+2. `pip install ursina`
+3. `python app.py`
+4. `Aproveite !`
+
+## Links
+
+Site principal Python: 
+https://www.python.org/ \
+Ursina: https://www.ursinaengine.org/
+
